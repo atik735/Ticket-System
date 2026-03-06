34f000b2adbf04be05aa73e4a0e7cd14ef873a9f
Atik Hassan <atikh01001@gmail.com>
2026-03-06 23:38:21 +0600

    task stats done

--- src/components/TaskStatus/TaskStatus.jsx
+++ src/components/TaskStatus/TaskStatus.jsx
@@ -6,7 +6,7 @@ export default function TaskStatus({ticketList,handleResolve}) {
      {
         ticketList.map((singleTicket=><div className="px-5 py-3 bg-white mt-5 w-full ">
                
-                <div className="card w-full space-y-2">
+                <div className="card w-full space-y-2 ">
                  <h1>{singleTicket.title}</h1>
                 <button onClick={()=>handleResolve(singleTicket)} className="px-28 py-2 bg-green-700 text-white ">Complete</button>
                 </div>
